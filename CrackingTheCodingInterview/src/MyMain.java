6da1248f759c404140910dbf93cbdd8ab584df8e
Lance Ji <lance@chcgp.com>
2017-04-17 18:22:11 -0700

    test

--- CrackingTheCodingInterview/src/MyMain.java
+++ CrackingTheCodingInterview/src/MyMain.java
@@ -2,10 +2,15 @@
  * Created by Lance on 4/17/17.
  */
 public class MyMain {
+    public void RandomNode(){
 
+    }
 
     public static void main(String[] args){
         MyMain mm = new MyMain();
-
+        mm.RandomNode();
+        int x  = 64;
+        System.out.println(x >> 1);
+        System.out.println(x >> 1);
     }
 }
