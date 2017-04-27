dbc527db6dcca832005f65d1aa7f3441fa430e63
Lance Ji <lance@chcgp.com>
2017-04-26 18:19:48 -0700

    test

--- CrackingTheCodingInterview/src/TestUnit.java
+++ CrackingTheCodingInterview/src/TestUnit.java
@@ -58,6 +58,62 @@ public class TestUnit {
         return num &mask;
        }
 
+    boolean OneAway(String str1, String str2){
+        int diff = Math.abs(str1.length() - str2.length());
+        if(diff >= 2)
+            return false;
+        else{
+            return checkOneWay(str1, str2, str1.length() - str2.length());
+        }
+    }
+
+    boolean checkOneWay(String str1, String str2, int lengthDiff){
+        int countDiff = 0;
+        int i=0,j=0;
+        while(i < str1.length() && j< str2.length()){
+            if(str1.charAt(i) != str2.charAt(j)){
+                countDiff++;
+                if(countDiff >= 2)
+                    return false;
+                if(lengthDiff == 0){
+                    i++;j++;
+                }else if(lengthDiff > 0){
+                    i++;
+                }else{
+                    j++;
+                }
+            }
+            else{
+                i++;j++;
+            }
+        }
+        return (countDiff <= 1);
+    }
+
+
+    String stringCompression(String input){
+        if(input==null || input.length() ==0)
+            return input;
+        char prev = input.charAt(0),cur='';
+        int index = 1,count = 1;
+        StringBuilder sb = new StringBuilder();
+        sb.append(prev);
+        while(index < input.length()){
+            cur = input.charAt(index);
+            if(cur == prev)
+                count++;
+            else{
+                sb.append(count);
+                sb.append(cur);
+                count = 1;
+                prev = cur;
+            }
+            index++;
+        }
+
+        return sb.length()>=input.length()?input:sb.toString();
+    }
+
     public static void main(String[] args){
         /*System.out.println(clearBitsMSBthroughI(15, 2));
         System.out.println(clearBitslthrough0(15, 2));*/
